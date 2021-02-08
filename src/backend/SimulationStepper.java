546b8f5fc0b53e9834c53b0bf54dc76ddd3578f0
Casey Szilagyi <crs79@duke.edu>
2021-02-08 16:43:34 -0500

    Added a class description

--- src/backend/SimulationStepper.java
+++ src/backend/SimulationStepper.java
@@ -1,2 +1,8 @@
-package backend;public class SimulationStepper {
+package backend;
+
+/** Deals with each "step" of the simulation, which is done every time the simulation
+ * loop makes a pass
+ */
+public class SimulationStepper {
+
 }
