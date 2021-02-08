81d21cbdc7b5406c1c4978441f9ed34dec417209
Casey Szilagyi <crs79@duke.edu>
2021-02-08 16:44:34 -0500

    Added a couple of the most basic classes and methods that will be necessary to run the simulation

--- src/backend/Simulation.java
+++ src/backend/Simulation.java
@@ -1,2 +1,29 @@
-package backend;public class Simulation {
+package backend;
+
+/**
+ * This class contains the game loop to run the simulation. It initializes the simulation, and
+ * contains the loop that runs the simulation. This loop will make a step, check for errors, check
+ * if the simulation is over (has reached an end state), and then pass the data to the front end
+ * through a helper class
+ *
+ * @author Casey Szilagyi
+ */
+public class Simulation {
+
+  SimulationInitializer simulationInitializer = new SimulationInitializer();
+
+  public Simulation(){
+    initializeSimulation();
+    makeSimulationLoop();
+  }
+
+  public void initializeSimulation(){
+    simulationInitializer.readXMLFile();
+  }
+
+  public void makeSimulationLoop(){
+
+  }
+
+
 }
