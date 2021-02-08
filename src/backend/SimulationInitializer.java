db842bd6e075f0e9a757b38315cafd12122f79e8
Casey Szilagyi <crs79@duke.edu>
2021-02-08 16:44:52 -0500

    Added the constructor and a basic method

--- src/backend/SimulationInitializer.java
+++ src/backend/SimulationInitializer.java
@@ -1,2 +1,17 @@
-package backend;public class SimulationInitializer {
+package backend;
+
+/**
+ * This is where all of the logic to initialize a simulation is contained. The XML file will be read
+ * in, cells will be initialized, their neighbors will be determined, and the grid will be made.
+ */
+public class SimulationInitializer {
+
+  public SimulationInitializer(){
+
+  }
+
+  public void readXMLFile(){
+
+  }
+
 }
