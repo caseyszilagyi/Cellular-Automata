475620cb90eec819c11a011689f2b2ff5eda8f5e
Casey Szilagyi <crs79@duke.edu>
2021-02-09 22:30:03 -0500

    Added methods from XMLparser from example roulette and changed it for my purppose. Hasn't been tested yet, but it compiles

--- src/cell_society/backend/SimulationInitializer/XMLFileReader.java
+++ src/cell_society/backend/SimulationInitializer/XMLFileReader.java
@@ -1,11 +1,18 @@
 package cell_society.backend.SimulationInitializer;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
 
 /** Used to take in an XML file and parse through it in order to get the information needed to
  * begin a simulation
@@ -15,19 +22,30 @@ import org.xml.sax.ErrorHandler;
  */
 public class XMLFileReader {
 
-  private ErrorHandler errorHandler = new XMLErrorHandler();
-  private DocumentBuilder db;
+  private final DocumentBuilder DOCUMENT_BUILDER;
+  private final String SIMULATION_TYPE;
   private Document doc;
 
   private String fileName;
 
+  public static final String ERROR_MESSAGE = "The file presented is not an XML file of the correct game type";
+
+  public final List<String> DATA_FIELDS = List.of(
+      "title",
+      "author",
+      "description",
+      "rows",
+      "columns",
+      "grid"
+  );
+
   /**
    * Constructor only takes a filename, because that is all that is needed. This assumes that all XML
    * files that are designed for the simulation have the proper tags.
-   * @param userFile the file name
    */
-  public XMLFileReader(String userFile){
-    fileName = userFile;
+  public XMLFileReader(String simulationType){
+    DOCUMENT_BUILDER = getDocumentBuilder();
+    SIMULATION_TYPE = simulationType;
   }
 
 
@@ -36,18 +54,78 @@ public class XMLFileReader {
    * @throws Exception if anything goes wrong while parsing
    */
   public void parseFile() throws Exception {
-    doc = db.parse(new File(fileName));
+    doc = DOCUMENT_BUILDER.parse(new File(fileName));
   }
 
 
+
+
   /**
-   * Makes the document builder that will be used to parse through all XML files
-   * @throws ParserConfigurationException
+   * Get data contained in this XML file as an object
    */
-  private void makeDocumentBuilder() throws ParserConfigurationException {
-    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    db = dbf.newDocumentBuilder();
-    db.setErrorHandler(errorHandler);
+  public Map getSimulationParameters (File dataFile) throws XMLErrorHandler {
+    Element root = getRootElement(dataFile);
+    if (! isValidFile(root, SIMULATION_TYPE)) {
+      throw new XMLErrorHandler(ERROR_MESSAGE, SIMULATION_TYPE);
+    }
+    // read data associated with the fields given by the object
+    Map<String, String> results = new HashMap<>();
+    for (String field : DATA_FIELDS) {
+      results.put(field, getTextValue(root, field));
+    }
+    return results;
+  }
+
+
+  // returns if this is a valid XML file for the specified object type
+  private boolean isValidFile (Element root, String type) {
+    return getAttribute(root, SIMULATION_TYPE).equals(type);
+  }
+
+  // get value of Element's attribute
+  private String getAttribute (Element e, String attributeName) {
+    return e.getAttribute(attributeName);
+  }
+
+  // get value of Element's text
+  private String getTextValue (Element e, String tagName) {
+    NodeList nodeList = e.getElementsByTagName(tagName);
+    if (nodeList != null && nodeList.getLength() > 0) {
+      return nodeList.item(0).getTextContent();
+    }
+    else {
+      // FIXME: empty string or exception? In some cases it may be an error to not find any text
+      return "";
+    }
+  }
+
+
+
+
+  // get root element of an XML file
+  private Element getRootElement (File xmlFile) throws XMLErrorHandler {
+    try {
+      DOCUMENT_BUILDER.reset();
+      Document xmlDocument = DOCUMENT_BUILDER.parse(xmlFile);
+      return xmlDocument.getDocumentElement();
+    }
+    catch (SAXException | IOException e) {
+      throw new XMLErrorHandler(e);
+    }
+  }
+
+
+
+
+
+  // boilerplate code needed to make a documentBuilder
+  private DocumentBuilder getDocumentBuilder () throws XMLErrorHandler {
+    try {
+      return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    }
+    catch (ParserConfigurationException e) {
+      throw new XMLErrorHandler(e);
+    }
   }
 
 }
