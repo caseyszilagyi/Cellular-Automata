8b11a87ddd6bf00492a9134869c911dc0b1e109e
Casey Szilagyi <crs79@duke.edu>
2021-02-10 18:39:12 -0500

    made it so that the cell parameters are read in properly

--- src/cell_society/backend/SimulationInitializer/SimulationInitializer.java
+++ src/cell_society/backend/SimulationInitializer/SimulationInitializer.java
@@ -2,6 +2,7 @@ package cell_society.backend.SimulationInitializer;
 
 
 
+import cell_society.backend.automata.Grid;
 import java.util.Map;
 
 /**
@@ -13,20 +14,32 @@ public class SimulationInitializer {
   private XMLFileReader xmlFileReader;
   private Map<String,String> simulationParameters;
   private Map<String,String> cellBehavior;
+  private GridCreator gridCreator;
 
   public SimulationInitializer() {
+
     xmlFileReader = new XMLFileReader();
   }
 
   public void initializeSimulation(String simulationType, String fileName) {
     xmlFileReader.setSimulationType(simulationType);
-     simulationParameters = xmlFileReader.getSimulationParameters(fileName);
-     cellBehavior = xmlFileReader.getCellBehavior(fileName);
+    simulationParameters = xmlFileReader.getSimulationParameters(fileName);
+    cellBehavior = xmlFileReader.getCellBehavior(fileName);
+    makeGrid();
+  }
+
+  public Grid makeGrid(){
+    gridCreator = new GridCreator(Integer.parseInt(simulationParameters.get("rows")),
+        Integer.parseInt(simulationParameters.get("columns")),
+        simulationParameters.get("grid"));
+
+    return gridCreator.getGrid();
   }
 
   public static void main(String[] args){
     SimulationInitializer mySim = new SimulationInitializer();
     mySim.initializeSimulation("gameOfLife", "data/config_files/testingFile.xml");
+
   }
 
 }
