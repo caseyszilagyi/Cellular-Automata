ecfb588f3819f4f351c3f4a9e065439601fb6e0e
Casey Szilagyi <crs79@duke.edu>
2021-02-10 18:46:58 -0500

    added some framework/methods that will be needed to make the grid

--- src/cell_society/backend/SimulationInitializer/GridCreator.java
+++ src/cell_society/backend/SimulationInitializer/GridCreator.java
@@ -6,10 +6,20 @@ import cell_society.backend.automata.Grid;
 public class GridCreator {
 
   private Grid gameGrid;
+  private CellInitializer cellInitializer;
 
-  public GridCreator(int row, int col, String grid){
+  public GridCreator(int row, int col, String grid, String gameType){
     gameGrid = new Grid(row, col);
+    makeCellInitializer(gameType);
+    populateGrid();
+  }
 
+  public void populateGrid(){
+
+  }
+
+  public void makeCellInitializer(String gameType){
+    cellInitializer = new CellInitializer();
   }
 
   public Grid getGrid(){
