ea8e4c07737b561b691b8f7aa7ffab43e8368ff0
George Hong <grh17@duke.edu>
2021-02-10 23:52:18 -0700

    May have to correct assumption about agent movement
    A third stage may need to be considered where all dissatisfied cells can
    have the ability to take each other's position

--- src/cell_society/backend/automata/segregation/AgentCell.java
+++ src/cell_society/backend/automata/segregation/AgentCell.java
@@ -37,6 +37,10 @@ public class AgentCell extends Cell {
   }
 
 
+  /**
+   * @param grid grid holding the current configuration of cells
+   * @return
+   */
   @Override
   public Neighbors getNeighbors(Grid grid) {
     int row = getRow();
@@ -44,6 +48,16 @@ public class AgentCell extends Cell {
     return grid.getDirectNeighbors(row, col);
   }
 
+  /**
+   * Allows the cell to determine whether it is satisfied with its current location.  A satisfied
+   * agent is surrounded by at least (satisfactionProp) agents that are like itself.  A dissatisfied
+   * agent will attempt to relocate to any free location on the grid.  A satisfied agent will remain
+   * in place.
+   *
+   * @param neighbors   Cells that this cell uses to make its decision
+   * @param nextGrid    grid to hold the next configuration of cells.
+   * @param currentGrid
+   */
   @Override
   public void makeDecisions(Neighbors neighbors, Grid nextGrid,
       Grid currentGrid) {
@@ -57,19 +71,26 @@ public class AgentCell extends Cell {
     }
     double similarNeighborsProp = (double) similarNeighbors / totalNeighbors;
 
-    if (similarNeighborsProp >= satisfactionProp) {
-      int row = getRow();
-      int col = getCol();
-      AgentCell agentCell = new AgentCell(row, col, satisfactionProp, agentType);
-      nextGrid.placeCell(row, col, agentCell);
-    } else {
-      moveToVacantSpot(currentGrid, nextGrid, this);
+    if (similarNeighborsProp < satisfactionProp && moveToVacantSpot(currentGrid, nextGrid, this)) {
+      return;
     }
+    int row = getRow();
+    int col = getCol();
+    AgentCell agentCell = new AgentCell(row, col, satisfactionProp, agentType);
+    nextGrid.placeCell(row, col, agentCell);
   }
 
-  public void moveToVacantSpot(Grid current, Grid next, AgentCell agent) {
+  /**
+   * Helper method for the AgentCell to attempt to move to a new location, if available.
+   *
+   * @param current current Grid configuration
+   * @param next    Grid configuration for the next state
+   * @param agent   agent's information to bring to the next state.
+   * @return whether or not the agent was able to successfully move to a new location.
+   */
+  public boolean moveToVacantSpot(Grid current, Grid next, AgentCell agent) {
     /*
-    If the agent chooses to move to a vacant spot, it must find a currently available spot, and it cannot conflict with the choices of another agent that chooses to move too in a round.  Does nothing if no choices are available.
+    If the agent chooses to move to a vacant spot, it must find a currently available spot on the board, and it cannot conflict with the choices of another agent that has already chosen to move too in a round.  The agent will choose to stay in place if no new position is available, writing to the next Grid state appropriately.
      */
     List<Coordinate> potentialSpots = current.getAllVacantSpots();
     while (potentialSpots.size() > 0) {
@@ -80,9 +101,11 @@ public class AgentCell extends Cell {
       if (next.isEmpty(r, c)) {
         AgentCell newAgentCell = new AgentCell(r, c, agent.satisfactionProp, agent.agentType);
         next.placeCell(r, c, newAgentCell);
+        return true;
       } else {
         potentialSpots.remove(coordinate);
       }
     }
+    return false;
   }
 }
