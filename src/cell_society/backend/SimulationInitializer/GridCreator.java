0bfe39780703b1628938f926b5b05eb1d7c578f5
Casey Szilagyi <crs79@duke.edu>
2021-02-11 00:23:43 -0500

    just added a comment to describe future implementation ideas

--- src/cell_society/backend/SimulationInitializer/GridCreator.java
+++ src/cell_society/backend/SimulationInitializer/GridCreator.java
@@ -3,21 +3,37 @@ package cell_society.backend.SimulationInitializer;
 import cell_society.backend.automata.Cell;
 import cell_society.backend.automata.Grid;
 
+/**
+ * Makes the grid that is used to hold the cells
+ *
+ * @author Casey Szilagyi
+ */
 public class GridCreator {
 
   private Grid gameGrid;
   private CellInitializer cellInitializer;
 
-  public GridCreator(int row, int col, String grid, String gameType){
+  /**
+   * Makes the grid
+   * @param row Number of rows
+   * @param col Number of columns
+   */
+  public GridCreator(int row, int col){
     gameGrid = new Grid(row, col);
-    makeCellInitializer(gameType);
-    populateGrid();
   }
 
-  public void populateGrid(){
+  /**
+   * Puts all of the cells in the grid
+   */
+  public void populateGrid(String grid){
 
   }
 
+  /** May not need this. Instead what might be done is a type of key will be added in
+   * the xml file that specifies what numbers in the grid correspond to a specific cell type.
+   * Then, Class.forName can be used to initialize a cell.
+   * @param gameType
+   */
   public void makeCellInitializer(String gameType){
     cellInitializer = new CellInitializer();
   }
