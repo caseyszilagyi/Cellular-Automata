12b25d62091dcc2694b86c6b43f1d0e953b9704e
Casey Szilagyi <crs79@duke.edu>
2021-02-11 22:15:44 -0500

    worked on initializer, still having an issue

--- src/cell_society/backend/SimulationInitializer/GridCreator.java
+++ src/cell_society/backend/SimulationInitializer/GridCreator.java
@@ -1,6 +1,8 @@
 package cell_society.backend.SimulationInitializer;
 
+import cell_society.backend.Simulation;
 import cell_society.backend.automata.Cell;
+import cell_society.backend.automata.game_of_life.DeadCell;
 import cell_society.backend.automata.Grid;
 import java.util.Map;
 
@@ -13,15 +15,19 @@ public class GridCreator {
 
   private Grid gameGrid;
   private Map<String,String> cellBehavior;
+  private String CELL_LOCATION = "cell_society.backend.automata.";
+  private String SIMULATION_TYPE;
 
+  DeadCell deadCell = new DeadCell();
 
   /**
    * Makes the grid
    * @param row Number of rows
    * @param col Number of columns
    */
-  public GridCreator(int row, int col){
+  public GridCreator(int row, int col, String simulationType){
     gameGrid = new Grid(row, col);
+    SIMULATION_TYPE = simulationType + ".";
   }
 
   /**
@@ -30,16 +36,19 @@ public class GridCreator {
   public void populateGrid(String grid, Map<String, String> cellCodes){
     grid = parseGrid(grid);
     int i = 0;
-    for(int c = 0; c<4; c++){
-      for(int r = 0; r<4; r++){
-        String curr = Character.toString(grid.charAt(i++));
+    for(int c = 0; c< gameGrid.getGridWidth(); c++){
+      for(int r = 0; r< gameGrid.getGridHeight(); r++){
+        String cellType = cellCodes.get(Character.toString(grid.charAt(i)));
+        String curr = CELL_LOCATION + SIMULATION_TYPE + cellType;
+        Class<Cell> currCell = null;
         try {
-          Class currCell = Class.forName(cellCodes.get(curr));
+          currCell = (Class<Cell>) Class.forName(curr);
         }  catch (ClassNotFoundException e){
           System.out.println("Class name does not exist");
         }
 
-
+        //Cell cell = new Cell();
+        //cell = (Cell) currCell;
 
       }
     }
