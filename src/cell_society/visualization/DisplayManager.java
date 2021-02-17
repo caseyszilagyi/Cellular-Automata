97b18ca40551f3ce4a0f313ff7b32e1619d3fe6a
Donghan Park <donghan.park@duke.edu>
2021-02-17 03:36:53 -0500

    removed Group import

--- src/cell_society/visualization/DisplayManager.java
+++ src/cell_society/visualization/DisplayManager.java
@@ -4,7 +4,6 @@ import cell_society.backend.Simulation;
 import java.io.File;
 import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
