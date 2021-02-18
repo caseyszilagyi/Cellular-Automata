b3a298768d122ee5abad7b86a522e066351aa67f
George Hong <grh17@duke.edu>
2021-02-18 01:37:04 -0700

    JavaDoc comments for CellStructure and Direction
    Comments to explain the overall structure of the Cell neighborhoods and
    directions.

--- src/cell_society/backend/automata/CellStructure.java
+++ src/cell_society/backend/automata/CellStructure.java
@@ -2,10 +2,33 @@ package cell_society.backend.automata;
 
 import java.util.List;
 
+/**
+ * This class exists for a Grid to easily reference the cell neighborhood properties.  For each Cell
+ * Structure class, a corresponding enum that implements Direction must be created to encode the
+ * directions.  This class is designed to be used in conjunction with the bounds-checking
+ * functionality of the Grid class.
+ */
 public abstract class CellStructure {
 
+  /**
+   * Provides the standard neighborhood of a given type of cell.  The list can be freely modified by
+   * the caller because coordinates are generated at each call.
+   *
+   * @param row row index of the desired cell at the center of each neighborhood
+   * @param col column index of the desired cell at the center of each neighborhood
+   * @return List of coordinates that contain each Coordinate of neighbors
+   */
   public abstract List<Coordinate> getAllNeighboringCoordinates(int row, int col);
 
+  /**
+   * Provides all adjacent neighbors of a given type of cell.  This List will be a subset of all
+   * neighbors.  The returned list can be freely modified by the caller because coordinates are
+   * generated at each call.
+   *
+   * @param row row index of the desired cell at the center of each neighborhood
+   * @param col column index of the desired cell at the center of each neighborhood
+   * @return List of coordinates that contain each Coordinate of neighbors
+   */
   public abstract List<Coordinate> getAllAdjacentCoordinates(int row, int col);
 
 
--- src/cell_society/backend/automata/grid_styles/Direction.java
+++ src/cell_society/backend/automata/grid_styles/Direction.java
@@ -3,9 +3,26 @@ package cell_society.backend.automata.grid_styles;
 import cell_society.backend.automata.Coordinate;
 import java.util.List;
 
+/**
+ * This interface allows the programmer to define the discrete set of directions relevant in each
+ * type of cell grid.  It provides many helper methods to the CellStructure class and centralizes
+ * definitions for adjacent directions and neighborhoods.
+ */
 public interface Direction {
-  public Coordinate getResultingCoordinate(int row, int col);
-  public List<Direction> getAdjacentDirections(int row, int col);
-  public Direction rotateLeft(Direction direction);
-  public Direction rotateRight(Direction direction);
+
+  /**
+   * Convenience method to quickly compute the coordinate of the point pointed to by a given
+   * direction, in the neighborhood centered at (row, col)
+   *
+   * @param row row index where the neighborhood is centered
+   * @param col column index where the neighborhood is centered
+   * @return Coordinate of the point pointed to by a given direction
+   */
+  Coordinate getResultingCoordinate(int row, int col);
+
+  List<Direction> getAdjacentDirections(int row, int col);
+
+  Direction rotateLeft(Direction direction);
+
+  Direction rotateRight(Direction direction);
 }
