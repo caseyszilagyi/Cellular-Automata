4db5560e7c310238d241971841cdd09a1e05ca74
George Hong <grh17@duke.edu>
2021-02-20 12:07:13 -0700

    DeadCell padding added
    This addition should hopefully help with the infinite grid.

--- src/cell_society/backend/automata/game_of_life/DeadCell.java
+++ src/cell_society/backend/automata/game_of_life/DeadCell.java
@@ -2,8 +2,10 @@ package cell_society.backend.automata.game_of_life;
 
 
 import cell_society.backend.automata.Cell;
+import cell_society.backend.automata.Coordinate;
 import cell_society.backend.automata.Grid;
 import cell_society.backend.automata.Neighbors;
+import java.util.List;
 
 /**
  * The DeadCell represents the dead cell in Conway's Game of Life subject to the following rule:
@@ -30,7 +32,8 @@ public class DeadCell extends Cell {
   /**
    * Updates the next Grid state according to the rules obeyed by DeadCells: Any dead cell with
    * exactly three live neighbours becomes a live cell, as if by reproduction.
-   *  @param neighbors   Cells that this cell uses to make its decision
+   *
+   * @param neighbors   Cells that this cell uses to make its decision
    * @param currentGrid
    * @param nextGrid    grid to hold the next configuration of cells.
    */
@@ -46,7 +49,25 @@ public class DeadCell extends Cell {
       DeadCell deadCell = new DeadCell(row, col);
       nextGrid.placeCell(row, col, deadCell);
     }
+
+    padDeadCells(currentGrid, nextGrid, numLiveNeighbors, row, col);
   }
+
+  private void padDeadCells(Grid currentGrid, Grid nextGrid, int numLiveNeighbors, int row,
+      int col) {
+    if (numLiveNeighbors > 0) {
+      List<Coordinate> coordinates = currentGrid.getGridCellStructure()
+          .getAllNeighboringCoordinates(row, col);
+      for (Coordinate coordinate : coordinates) {
+        int coordRow = coordinate.getFirst();
+        int coordCol = coordinate.getSecond();
+        if (nextGrid.inBoundaries(coordRow, coordCol) && nextGrid.isEmpty(coordRow, coordCol)) {
+          nextGrid.placeCell(coordRow, coordCol, new DeadCell(coordRow, coordCol));
+        }
+      }
+    }
+  }
+
 
   @Override
   public String toString() {
