73bcd8681597bba2c5d4082e7836cb66b0f054da
George Hong <grh17@duke.edu>
2021-02-20 16:43:25 -0700

    A few JavaDoc comments for the Infinite Grid

--- src/cell_society/backend/automata/grid_styles/InfiniteGrid.java
+++ src/cell_society/backend/automata/grid_styles/InfiniteGrid.java
@@ -8,23 +8,48 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The infinite grid allows cells to move to whatever coordinate they desire.  The grid initially
+ * has a fixed size, but expands as cells move out of the initial boundaries.  To view the grid,
+ * slices of the grid can be observed, with (0, 0) as the reference point.
+ */
 public class InfiniteGrid extends Grid {
 
   // These should override the variables of the base class
   private final Map<Coordinate, Cell> grid = new HashMap<>();
   private final Map<Coordinate, Patch> gridStates = new HashMap<>();
 
+  /**
+   * Constructs an instance of the Infinite Grid
+   *
+   * @param gridHeight initial height of the infinite grid
+   * @param gridWidth  initial width of the infinite grid
+   */
   public InfiniteGrid(int gridHeight, int gridWidth) {
     super(gridHeight, gridWidth);
   }
 
+  /**
+   * Copy constructor of the Infinite Grid intended for use with a stepper.
+   *
+   * @param grid previous grid state to copy
+   */
   public InfiniteGrid(Grid grid) {
     super(grid);
   }
 
+  /**
+   * Parameter-less constructor intended for use with the XML file reader.
+   */
   public InfiniteGrid() {
   }
 
+  /**
+   * Method intended for use with the parameter-less constructor.
+   *
+   * @param width  The grid width
+   * @param height The grid height
+   */
   @Override
   public void makeGrid(int width, int height) {
     super.makeGrid(width, height);
@@ -50,6 +75,14 @@ public class InfiniteGrid extends Grid {
     gridStates.put(new Coordinate(row, col), patch);
   }
 
+  /**
+   * Checks that a (row, col) coordinate pair is within boundaries.  This is always true because the
+   * Infinite Grid can accommodate any coordinate pair.
+   *
+   * @param row
+   * @param col
+   * @return
+   */
   @Override
   public boolean inBoundaries(int row, int col) {
     return true;
@@ -60,6 +93,13 @@ public class InfiniteGrid extends Grid {
     return grid.get(new Coordinate(row, col)) == null;
   }
 
+  /**
+   * Returns the list of coordinates corresponding to Cells within the grid.  These are cells that
+   * the stepper must update in the course of a step.  These coordinates are generated on call, and
+   * can be mutated, as desired.
+   *
+   * @return List of coordinates.
+   */
   @Override
   public List<Coordinate> getCoordinateUpdateList() {
     List<Coordinate> coordinateList = new ArrayList<>();
@@ -69,6 +109,12 @@ public class InfiniteGrid extends Grid {
     return coordinateList;
   }
 
+  /**
+   * Lifts patches from the previous Grid into this grid state.  A typical use for this method is at
+   * the beginning of the update step.
+   *
+   * @param otherGrid previous Grid object
+   */
   @Override
   public void updateRemainingPatches(Grid otherGrid) {
     //List<Coordinate> coordinateList = getCoordinateUpdateList();
