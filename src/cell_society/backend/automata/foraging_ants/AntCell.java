d9ab4fbfc6d727e3b71c699f3984b333ebbe42b7
George Hong <grh17@duke.edu>
2021-02-21 12:55:15 -0700

    Back to editing AntCells
    Need to select a facing direction.

--- src/cell_society/backend/automata/foraging_ants/AntCell.java
+++ src/cell_society/backend/automata/foraging_ants/AntCell.java
@@ -6,13 +6,17 @@ import cell_society.backend.automata.Neighbors;
 import cell_society.backend.automata.Patch;
 import cell_society.backend.automata.grid_styles.Direction;
 import cell_society.backend.automata.grid_styles.Grid;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AntCell extends Cell {
 
-  private final Direction antDirection;
+  private Direction antDirection;
   private final int food;
+
+  public AntCell(int row, int col, AntCell antCell) {
+    super(row, col);
+    antDirection = antCell.antDirection;
+    food = antCell.food;
+  }
 
   public AntCell(int row, int col, Grid grid) {
     super(row, col);
@@ -27,32 +31,22 @@ public class AntCell extends Cell {
 
   @Override
   public void performPrimaryAction(Neighbors neighbors, Grid currentGrid, Grid nextGrid) {
+    // Find food mode
     if (food == 0) {
       int row = getRow();
       int col = getCol();
-      List<Coordinate> availableDirections = new ArrayList<>();
-      Coordinate directionCW = antDirection.rotateCW().getResultingCoordinate(row, col);
-      Patch patchCW = currentGrid.inBoundaries(row, col) ? null : currentGrid.getPatch(
-          directionCW.getFirst(), directionCW.getSecond());
-      Coordinate directionCCW = antDirection.rotateCCW().getResultingCoordinate(row, col);
-      Patch patchCCW = currentGrid.inBoundaries(row, col) ? null : currentGrid.getPatch(
-          directionCCW.getFirst(), directionCCW.getSecond());
-      Coordinate chosenCoordinate;
-      if (patchCW == null && patchCCW == null) {
-        // TODO: Choose new coordinate and leave
-
-      } else if (patchCW == null) {
-        chosenCoordinate = directionCCW;
-      } else if (patchCCW == null) {
-        chosenCoordinate = directionCW;
-      } else {
-        // TODO: Use randomized coordinate choosing
-      }
+      // Assess movement options
 
 
     } else {
 
     }
+  }
+
+  private boolean validDirection(int row, int col, Direction direction, Grid currentGrid, Grid nextGrid) {
+    // A valid cell direction choice if a cell can move to it.
+    Coordinate result = direction.getResultingCoordinate(row, col);
+    return nextGrid.inBoundaries(result) && currentGrid.isEmpty(result) && nextGrid.isEmpty(result);
   }
 
   @Override
