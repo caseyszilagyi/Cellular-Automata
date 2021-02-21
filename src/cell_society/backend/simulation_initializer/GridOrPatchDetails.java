45f848c0cf9cb6ef860f45b69349b4f149d6db6f
Casey Szilagyi <crs79@duke.edu>
2021-02-21 15:51:03 -0500

    Added random configuration generator

--- src/cell_society/backend/simulation_initializer/GridOrPatchDetails.java
+++ src/cell_society/backend/simulation_initializer/GridOrPatchDetails.java
@@ -1,28 +1,53 @@
 package cell_society.backend.simulation_initializer;
 
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Random;
+import java.util.TreeMap;
 
 public class GridOrPatchDetails {
 
   private String TYPE;
   private int GRID_HEIGHT;
   private int GRID_WIDTH;
-  private String GRID;
+  private String grid;
   private Map<String, String> PARAMETERS;
   private Map<String, String> CODES;
   private Map<String, String> DECODER;
+  private Map<String, String> PROBABILITIES;
 
   private String DEFAULT_PATH = "data/default_values/";
 
-  public GridOrPatchDetails(String type, int width, int height, String grid,
-      Map<String, String> parameters, Map<String, String> codes, Map<String, String> decoder) {
+  public GridOrPatchDetails(String type, int width, int height, String userGrid,
+      Map<String, String> parameters, Map<String, String> codes, Map<String, String> decoder, Map<String, String> probabilities) {
     TYPE = type;
     GRID_WIDTH = width;
     GRID_HEIGHT = height;
-    GRID = grid;
+    grid = userGrid;
     PARAMETERS = parameters;
     CODES = codes;
     DECODER = decoder;
+    PROBABILITIES = probabilities;
+    setUpGrid();
+  }
+
+  private void setUpGrid(){
+    if(grid.replace("\n", "").replace(" ", "").equals("Random")){
+      StringBuilder sb = new StringBuilder();
+      RandomCollection<String> weights = populateWeights();
+      for(int i = 0; i<GRID_HEIGHT*GRID_WIDTH; i++){
+        sb.append(weights.next());
+      }
+      grid = sb.toString();
+    }
+  }
+
+  private RandomCollection<String> populateWeights(){
+    RandomCollection<String> weights = new RandomCollection();
+    for(String s: PROBABILITIES.keySet()){
+      weights.add(Double.parseDouble(PROBABILITIES.get(s)), s);
+    }
+    return weights;
   }
 
   public String getType() {
@@ -38,7 +63,7 @@ public class GridOrPatchDetails {
   }
 
   public String getGrid() {
-    return GRID;
+    return grid;
   }
 
   public Map<String, String> getParameters() {
@@ -51,6 +76,33 @@ public class GridOrPatchDetails {
 
   public Map<String, String> getDecoder() {
     return DECODER;
+  }
+
+
+  public class RandomCollection<E> {
+    private final NavigableMap<Double, E> map = new TreeMap<Double, E>();
+    private final Random random;
+    private double total = 0;
+
+    public RandomCollection() {
+      this(new Random());
+    }
+
+    public RandomCollection(Random random) {
+      this.random = random;
+    }
+
+    public RandomCollection<E> add(double weight, E result) {
+      if (weight <= 0) return this;
+      total += weight;
+      map.put(total, result);
+      return this;
+    }
+
+    public E next() {
+      double value = random.nextDouble() * total;
+      return map.higherEntry(value).getValue();
+    }
   }
 
 }
